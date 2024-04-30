1c39dd998176fb757416087d1ab01479a9a0f043
wzz <74001219+Wangzhezhou@users.noreply.github.com>
2024-04-30 21:51:53 +1000

    change loss to epe loss, add more metrics (1px, 3px, 5px)

--- utils/dice_score.py
+++ utils/dice_score.py
@@ -2,27 +2,24 @@ import torch
 from torch import Tensor
 
 
-def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
-    # Average of Dice coefficient for all batches, or for a single mask
-    assert input.size() == target.size()
-    assert input.dim() == 3 or not reduce_batch_first
+MAX_FLOW = 400 
 
-    sum_dim = (-1, -2) if input.dim() == 2 or not reduce_batch_first else (-1, -2, -3)
+def epe_loss(input_flow, target_flow, valid):
+    """
+    - input_flow: (batch_size, 2, H, W) (means predict flow)
+    - target_flow: (batch_size, 2, H, W) (means GT flow)
+    - valid: (batch_size, H, W) indicating valid pixels
+    """
+    valid = (valid >= 0.5) & (torch.norm(target_flow, p=2, dim=1) < MAX_FLOW)
+    difference = target_flow - input_flow
+    epe = torch.norm(difference, p=2, dim=1)
+    valid_epe = epe[valid]
+    loss = valid_epe.mean()
 
-    inter = 2 * (input * target).sum(dim=sum_dim)
-    sets_sum = input.sum(dim=sum_dim) + target.sum(dim=sum_dim)
-    sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
-
-    dice = (inter + epsilon) / (sets_sum + epsilon)
-    return dice.mean()
-
-
-def multiclass_dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
-    # Average of Dice coefficient for all classes
-    return dice_coeff(input.flatten(0, 1), target.flatten(0, 1), reduce_batch_first, epsilon)
-
-
-def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False):
-    # Dice loss (objective to minimize) between 0 and 1
-    fn = multiclass_dice_coeff if multiclass else dice_coeff
-    return 1 - fn(input, target, reduce_batch_first=True)
+    metrics = {
+        'epe': valid_epe.mean().item(),
+        '1px': (valid_epe < 1).float().mean().item(),
+        '3px': (valid_epe < 3).float().mean().item(),
+        '5px': (valid_epe < 5).float().mean().item(),
+    }
+    return loss, metrics
