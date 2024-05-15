30ff6c9d7c276d5b71ddd31cb7367b34be8a2d2f
wzz <74001219+Wangzhezhou@users.noreply.github.com>
2024-05-15 12:26:43 +1000

    Update train.py

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from unet import UNet
 from utils.data_loading import BasicDataset
 from utils.dice_score import epe_loss
 
-dir_img = Path('./data/imgs/clean')
+dir_img = Path('./data/KITTI/image_2')
 dir_flow = Path('./data/flow/')
 dir_checkpoint = Path('./checkpoints/')
 
@@ -29,14 +29,14 @@ def train_model(
         device,
         epochs: int = 100,
         batch_size: int = 1,
-        learning_rate: float = 0.00002,
-        val_percent: float = 0.1,
+        learning_rate: float = 0.009526887711772588,
+        val_percent: float = 0.052267383182929415,
         save_checkpoint: bool = True,
         img_scale: float = 1.0,  # modify 2: original = .5
         amp: bool = False,
-        weight_decay: float = 1e-8,
-        momentum: float = 0.999,
-        gradient_clipping: float = 1.0,
+        weight_decay: float = 0.00001459482410533705,
+        momentum: float = 0.8,
+        gradient_clipping: float = 0.5,
 ):
     # 1. Create dataset
     dataset = BasicDataset(dir_img, dir_flow, img_scale)
@@ -73,6 +73,7 @@ def train_model(
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay, eps=1e-8)
+    '''
     scheduler = optim.lr_scheduler.OneCycleLR(
         optimizer,
         max_lr=learning_rate,
@@ -80,7 +81,10 @@ def train_model(
         pct_start=0.1,
         anneal_strategy='linear'
     )
-
+    '''
+    # modify: change scheduler to Cosine
+    # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs * 100, eta_min=2e-7)
 
     # optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
 
@@ -94,9 +98,15 @@ def train_model(
     for epoch in range(1, epochs + 1):
         model.train()
         epoch_loss = 0
+        batch_count = 0  
+        accumulated_loss = 0
+        total_1px_accuracy = 0
+        total_3px_accuracy = 0.0
+        total_5px_accuracy = 0.0
         with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
             for batch in train_loader:
-                images, flow, valids = batch['image'], batch['flow'], batch['valid']
+                # images, flow, valids = batch['image'], batch['flow'], batch['valid']
+                images, flow = batch['image'], batch['flow']
 
                 assert images.shape[1] == model.n_channels, \
                     f'Network has been defined with {model.n_channels} input channels, ' \
@@ -105,15 +115,16 @@ def train_model(
 
                 images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
                 flow = flow.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
-                valids = valids.to(device=device, dtype=torch.float32)
+                # valids = valids.to(device=device, dtype=torch.float32)
 
                 with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
                     flow_pred = model(images)
                     flow = flow.permute(0, 3, 1, 2)
 
-                    print("Predicted flow shape:", flow_pred.shape)
-                    print("True flow shape:", flow.shape)
-                    loss, metrics = epe_loss(flow_pred, flow, valids)
+                    # print("Predicted flow shape:", flow_pred.shape)
+                    # print("True flow shape:", flow.shape)
+                    # loss, metrics = epe_loss(flow_pred, flow, valids)
+                    loss, metrics = epe_loss(flow_pred, flow)
 
                 optimizer.zero_grad(set_to_none=True)
                 grad_scaler.scale(loss).backward()
@@ -122,26 +133,44 @@ def train_model(
                 grad_scaler.step(optimizer)
                 
                 # modify 1 (add)
-                scheduler.step()
+                # scheduler.step()
                 # end modify 1
                 grad_scaler.update()
 
                 pbar.update(images.shape[0])
                 global_step += 1
                 epoch_loss += loss.item()
-                experiment.log({
-                    'learning rate': optimizer.param_groups[0]['lr'],
-                    'train loss': loss.item(),
-                    '1px accuracy': metrics['1px'],
-                    '3px accuracy': metrics['3px'],
-                    '5px accuracy': metrics['5px'],
-                    'step': global_step,
-                    'epoch': epoch
-                })
-                pbar.set_postfix(**{'loss (batch)': loss.item()})
+                accumulated_loss += loss.item()
+                total_1px_accuracy += metrics['1px']
+                total_3px_accuracy += metrics['3px']
+                total_5px_accuracy += metrics['5px']
+                batch_count += 1
+
+                if batch_count == 10:
+                    average_loss = accumulated_loss / batch_count
+                    average_1px_accuracy = total_1px_accuracy / batch_count
+                    average_3px_accuracy = total_3px_accuracy / batch_count
+                    average_5px_accuracy = total_5px_accuracy / batch_count
+                    scheduler.step()
+                    experiment.log({
+                        'learning rate': optimizer.param_groups[0]['lr'],
+                        'average train loss': average_loss,
+                        '1px accuracy': average_1px_accuracy,
+                        '3px accuracy': average_3px_accuracy,
+                        '5px accuracy': average_5px_accuracy,
+                        'step': global_step,
+                        'epoch': epoch
+                    })
+                    epoch_loss += accumulated_loss
+                    accumulated_loss = 0
+                    total_1px_accuracy = 0
+                    total_3px_accuracy = 0.0
+                    total_5px_accuracy = 0.0
+                    batch_count = 0
+               
+                    pbar.set_postfix(**{'loss (batch)': average_loss})
 
                 # Evaluation round
-                '''
                 division_step = (n_train // (5 * batch_size))
                 if division_step > 0:
                     if global_step % division_step == 0:
@@ -153,16 +182,20 @@ def train_model(
                             if not (torch.isinf(value.grad) | torch.isnan(value.grad)).any():
                                 histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
 
-                        val_score = evaluate(model, val_loader, device, amp)
+                        # val_score = evaluate(model, val_loader, device, amp)
                         # modify 1 (delete)
                         # scheduler.step(val_score)
                         # end modify 1
+                        val_metrics = evaluate(model, val_loader, device, amp)
 
-                        logging.info('Validation epe score: {}'.format(val_score))
+                        logging.info('Validation EPE score: {:.2f}'.format(val_metrics['epe']))
                         try:
                             experiment.log({
                                 'learning rate': optimizer.param_groups[0]['lr'],
-                                'validation epe score': val_score,
+                                'validation epe score': val_metrics['epe'],
+                                '1px accuracy': val_metrics['1px_accuracy'],
+                                '3px accuracy': val_metrics['3px_accuracy'],
+                                '5px accuracy': val_metrics['5px_accuracy'],
                                 'images': wandb.Image(images[0].cpu()),
                                 'step': global_step,
                                 'epoch': epoch,
@@ -170,7 +203,6 @@ def train_model(
                             })
                         except:
                             pass
-                    '''
 
         if save_checkpoint:
             Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
@@ -181,18 +213,22 @@ def train_model(
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=100, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.00002,
+    parser.add_argument('--batch_size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
+    parser.add_argument('--learning_rate', '-l', metavar='LR', type=float, default=0.009526887711772588,
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
     parser.add_argument('--scale', '-s', type=float, default=1.0, help='Downscaling factor of the images') # modify 2: change scale 0.5 -> 1.0
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
+    parser.add_argument('--validation', '-v', dest='val', type=float, default=5.2267383182929415,
                         help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
+    parser.add_argument('--momentum', type=float, default=0.8, help='Momentum for optimizer')
+    parser.add_argument('--weight_decay', type=float, default=0.00001459482410533705, help='Weight decay for optimizer')
+    parser.add_argument('--gradient_clipping', type=float, default=0.5, help='Gradient clipping threshold')
     parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
 
     return parser.parse_args()
+
 if __name__ == '__main__':
     args = get_args()
 
@@ -227,7 +263,10 @@ if __name__ == '__main__':
             device=device,
             img_scale=args.scale,
             val_percent=args.val / 100,
-            amp=args.amp
+            amp=args.amp,
+            weight_decay=args.weight_decay,
+            momentum=args.momentum,
+            gradient_clipping=args.gradient_clipping
         )
     except torch.cuda.OutOfMemoryError:
         logging.error('Detected OutOfMemoryError! '
